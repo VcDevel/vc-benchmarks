f6d7bd2e57702d5b8a5070fb21e62d063933fcd6
LepelTsmok <bjoerngaier@web.de>
2016-03-14 14:22:40 +0100

    L2 and L3 Cache Test added; Little improvement of baseline calculation

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main()
 }
 #else
 
+//!Tests the L1-Cache sizes from my PC
 void sizesToL1CacheSize(benchmark::internal::Benchmark *function)
 {
  int sizes[] = {1, 2, 3, 4, 6, 8, 12, 16, 26, 32, 41, 53, 64, 70, 88, 109, 128, 177, 201, 256, 356, 566, 789, 1024, 1234, 1323, 1600, 1899, 2048, 2934, 3433, 3677, 4096, 4304, 4666, 4900, 5555, 7809, 8000, 8192};
@@ -30,7 +31,20 @@ void sizesToL1CacheSize(benchmark::internal::Benchmark *function)
         }
 }
 
-void (*applyFunction)(benchmark::internal::Benchmark*) = sizesToL1CacheSize;
+//!Tests the L2-Cache sizes form my PC
+void sizesToL2CacheSize(benchmark::internal::Benchmark *function)
+{
+ int sizes[] = {1, 2, 4, 6, 8, 16, 19, 24, 29, 32, 43, 54, 64, 75, 86, 97, 108, 119, 128, 139, 148, 157, 166, 175, 184, 193, 202, 213, 224, 235, 246, 256, 278, 300, 321, 343, 364, 385, 400, 433, 465, 499, 512, 544, 584, 633, 666, 1000, 1024, 1300, 1655, 1998, 2048, 2356, 2654, 2967, 3299, 3666, 3988, 4096, 6333, 8192, 9999,
+                12333, 14444, 16384, 18770, 22456, 26666, 30233, 32768, 38622, 42333, 48665, 53535, 60066, 62000, 65536};
+ size_t n;
+
+    for(n = 0; n < (sizeof(sizes)/sizeof(int)); n++)
+    {
+        function->Arg(sizes[n]);
+    }
+}
+
+void (*applyFunction)(benchmark::internal::Benchmark*) = sizesToL2CacheSize;
 
 //!Scalar
 //BENCHMARK(Scalar)                           ->Apply(applyFunction);
