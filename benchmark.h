72eaa65ab28af1393d37f0a1f66674fb38ed32ed
Matthias Kretz <kretz@kde.org>
2016-04-25 14:03:06 +0200

    Rename registermodification to fakereadmodify
    ... and include it from benchmark.h for all to use
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- benchmark.h
+++ benchmark.h
@@ -26,6 +26,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
 #include <benchmark/benchmark.h>
 #include <memory>
+#include "fakereadmodify.h"
 #include "typetostring.h"
 
 typedef std::unique_ptr<std::vector<::benchmark::internal::Benchmark *>>
