a86886dc9b371db1163dc74e1d24b550f5ec66a3
Matthias Kretz <kretz@kde.org>
2016-05-17 11:26:14 +0200

    Test more functions, shorter names, less overhead
    * shorten the test names
    * atan2 is a 2-argument function
    * use new Run(<lambda>) instead of KeepRunning loop for less overhead
    * benchmark all the defined math functions
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- arithmetics.cpp
+++ arithmetics.cpp
@@ -45,13 +45,13 @@ template <bool T> struct HasTwoOps { static constexpr bool hasTwoOps = T; };
     }                                                                                    \
   }
 
-Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Addition, +);
-Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Substraction, -);
-Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Multiplication, *);
-Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Division, / );
-Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Modulo, % );
-Vc_MAKE_ONE_OP_OPERATOR(Sine, Vc::sin);
-Vc_MAKE_ONE_OP_OPERATOR(Cosine, Vc::cos);
+Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Add, +);
+Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Sub, -);
+Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Mul, *);
+Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Div, / );
+Vc_MAKE_BASIC_ARITHMETIC_OPERATOR(Mod, % );
+Vc_MAKE_ONE_OP_OPERATOR(Sin, Vc::sin);
+Vc_MAKE_ONE_OP_OPERATOR(Cos, Vc::cos);
 Vc_MAKE_ONE_OP_OPERATOR(Sqrt, Vc::sqrt);
 Vc_MAKE_ONE_OP_OPERATOR(Rsqrt, Vc::rsqrt);
 Vc_MAKE_ONE_OP_OPERATOR(Reciprocal, Vc::reciprocal);
@@ -63,12 +63,9 @@ Vc_MAKE_ONE_OP_OPERATOR(Log10, Vc::log10);
 Vc_MAKE_ONE_OP_OPERATOR(Exp, Vc::exp);
 Vc_MAKE_ONE_OP_OPERATOR(Asin, Vc::asin);
 Vc_MAKE_ONE_OP_OPERATOR(Atan, Vc::atan);
-Vc_MAKE_ONE_OP_OPERATOR(Atan2, Vc::atan2);
+Vc_MAKE_TWO_OP_OPERATOR(Atan2, Vc::atan2);
 Vc_MAKE_TWO_OP_OPERATOR(Min, Vc::min);
 Vc_MAKE_TWO_OP_OPERATOR(Max, Vc::max);
-
-#define Vc_ALL_BASIC_ARITHMETICS                                                         \
-  Typelist<Addition, Substraction, Multiplication, Division>
 
 template <typename T, typename P>
 inline typename std::enable_if<P::hasTwoOps, T>::type calculate(T &x, T &y)
@@ -82,7 +79,6 @@ inline typename std::enable_if<!P::hasTwoOps, T>::type calculate(T &x, T &y)
   return P::template calculate<T>(x);
 }
 
-//! Addition of two vectors
 template <typename TT> void oneOp(benchmark::State &state) {
   using T = typename TT::template at<0>;
   using P = typename TT::template at<1>;
@@ -91,21 +87,18 @@ template <typename TT> void oneOp(benchmark::State &state) {
   T y = 1;
   T z;
 
-  while (state.KeepRunning()) {
+  state.Run([&] {
     fakeRegisterModification(x);
     fakeRegisterModification(y);
-
     z = calculate<T, P>(x, y);
-
     fakeRegisterRead(z);
-  }
-
+  });
   state.SetItemsProcessed(state.iterations() * T::size());
 }
 
 Vc_BENCHMARK_TEMPLATE(
-    oneOp,
-    concat<outer_product<Vc_ALL_VECTORS,
-                         Typelist<Addition, Substraction, Multiplication, Division>>,
-           outer_product<Vc_ALL_INT_VECTORS, Typelist<Modulo>>,
-           outer_product<Vc_ALL_FLOAT_VECTORS, Typelist<Sine, Cosine>>>);
+    oneOp, concat<outer_product<Vc_ALL_VECTORS, Typelist<Add, Sub, Mul, Div>>,
+                  outer_product<Vc_ALL_INT_VECTORS, Typelist<Mod>>,
+                  outer_product<Vc_ALL_FLOAT_VECTORS,
+                                Typelist<Sin, Cos, Sqrt, Rsqrt, Abs, Round, Log, Log2,
+                                         Log10, Exp, Asin, Atan, Atan2, Min, Max>>>);
